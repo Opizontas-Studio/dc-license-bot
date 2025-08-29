3b94325aa86a885d443e493e7b6b14cc675df985
yeshanjun <69859571+yeshanjun@users.noreply.github.com>
2025-08-30 00:05:10 +0200

    refactor: 移除未使用的事件处理程序

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use dc_bot::{
     config::BotCfg,
     database::BotDatabase,
     error::BotError,
-    handlers::*,
     services::{gateway, notification_service::NotificationService, system_license::SystemLicenseCache},
 };
 use serenity::{Client, all::GatewayIntents};
@@ -89,7 +88,6 @@ async fn main() -> Result<(), BotError> {
         })
         .type_map_insert::<BotDatabase>(db.to_owned())
         .type_map_insert::<BotCfg>(cfg.to_owned())
-        .event_handler(PingHandler)
         .framework(framework(
             db,
             cfg,
